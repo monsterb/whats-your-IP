1bbdaa4d7d6c450f36e5f88eea89167c3edb5964
m0nst3rb <unix.s3c@gmail.com>
2017-03-21 02:41:14 -0500

    Create whats-your-ip.php

---
+++ whats-your-ip.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<!-- 
+	Maintainer: monsterb
+	Contact: UNIX.S3C (at) gmail (dot) com
+	URL: http://monsterb.github.io/ -->
+<head>
+<title>monsterb | daemons, gnus, and penguins. Oh my! | What's your IP?</title>
+  </head>
+   <body>
+   
+   
+<?php echo "Your IP Address: ";
+		echo $_SERVER['REMOTE_ADDR'];
+		echo "<br /><br />Reported User Agent is: " ;
+		echo $_SERVER["HTTP_USER_AGENT"];
+	  ?>
+	  
+	  
+	  
+	  </body>
+	  </html>
